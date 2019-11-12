e98c3c53cdb96a245a2df1a40b6de2f11420b30e
Amanda-Ye <48435335+Amanda-Ye@users.noreply.github.com>
2019-11-12 14:20:36 +0800

    Update LongestString.java
    官方解题方法

--- 2019-11-08/src/LongestString.java
+++ 2019-11-08/src/LongestString.java
@@ -1,30 +1,24 @@
-import java.util.Map;
-import java.util.TreeMap;
+import java.*;
 
 //在一行内输出 str 中连续最长的数字串
 public class LongestString {
-    public static void longest(String s) {     //不能通过所有测试用例，方法有待考证
-        int max = 0;
-        int index = 0;
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-                count++;
-                if (max < count) {
-                    max = count;
-                    index = i;
-                }
-            } else {
-                count = 0;
-            }
-        }
-        for (int i = index - max + 1; i <= index; i++) {
-            System.out.print(s.charAt(i));
-        }
-        System.out.println();
-    }
     public static void main(String[] args) {
-        String s = "1234s2345s3456";
-        longest(s);
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            String str = scanner.nextLine();
+            int max = 0,count=0,end=0;
+            for(int i=0;i<str.length();i++){
+                if(str.charAt(i)>='0' && str.charAt(i)<='9'){
+                    count++;
+                    if(max<count){
+                        max= count;
+                        end = i;
+                    }
+                }else{
+                    count = 0;
+                }
+            }
+            System.out.println(str.substring(end-max+1,end+1));
+        }
     }
 }
